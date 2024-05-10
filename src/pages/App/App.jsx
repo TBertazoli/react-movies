ad0aa4f2f1ffc0f10cb87f223f0e2cdf95fc980a
Tatiana Bertazoli <tatianabertazoli@gmail.com>
2024-05-10 15:19:04 -0500

    added routes to the app component

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,11 +1,29 @@
+import { userState } from "react";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import ActorListPage from "../ActorListPage/ActorListPage";
 import LoginPage from "../LoginPage/LoginPage";
 import MovieDetailPage from "../MovieDetailPage/MovieDetailPage";
 import MoviesListPage from "../MoviesListPage/MoviesListPage";
+import NavBar from "../../components/NavBar/NavBar";
 
-function App() {
-  return <div className="App"></div>;
+export default function App() {
+  const [user, setUser] = userState(null);
+
+  return (
+    <main className="App">
+      {user ? (
+        <>
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<MoviesListPage />} />
+            <Route path="/movies/:movieName" element={<MovieDetailPage />} />
+            <Route path="/actors" element={<ActorListPage />} />
+          </Routes>
+        </>
+      ) : (
+        <LoginPage setUser={setUser} />
+      )}
+    </main>
+  );
 }
-
-export default App;
