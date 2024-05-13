475aba8f983ea1963945301354b1daee478dcea3
Tatiana Bertazoli <tatianabertazoli@gmail.com>
2024-05-13 17:06:08 -0500

    changed actor cards to look similar to wireframe

--- src/components/ActorCard/ActorCard.jsx
+++ src/components/ActorCard/ActorCard.jsx
@@ -2,31 +2,30 @@ import { Link } from "react-router-dom";
 
 export default function ActorCard({ actor, index }) {
   return (
-    <Link to={`/actors/${actor}`}>
-      <div style={{ display: "flex" }}>
-        <div>
-          {/* <img src="https://picsum.photos/200/300?random=1" alt="actor" /> */}
-          <div
-            style={{
-              backgroundImage: `url(https://picsum.photos/200/300?random=${index})`,
-              backgroundPosition: "center",
-              backgroundRepeat: "no-repeat",
-              height: "30vh",
-              width: "30vw",
-              backgroundSize: "cover",
-              margin: "1vh",
-              border: "0.5px  solid rgba(150, 150, 150, 0.5)",
-            }}
-          >
-            <h1
-              style={{
-                backgroundColor: "rgba(255, 255, 255, 0.5)",
-                borderRadius: "4px",
-              }}
-            >
-              {actor}
-            </h1>
-          </div>
+    <Link
+      to={`/actors/${actor}`}
+      style={{ textDecoration: "none", color: "black" }}
+    >
+      <div
+        style={{
+          backgroundImage: `url(https://picsum.photos/200/300?random=${index})`,
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+          height: "30vh",
+          width: "30vw",
+          border: "0.5px  solid rgba(150, 150, 150, 0.5)",
+          margin: "1vh",
+          borderRadius: "5px",
+          backgroundSize: "cover",
+        }}
+      >
+        <div
+          style={{
+            backgroundColor: "rgba(255, 255, 255, 0.5)",
+            borderRadius: "4px",
+          }}
+        >
+          <h1>{actor}</h1>
         </div>
       </div>
     </Link>
